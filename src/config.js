4309f8d936f595b8278500fb70fbc01f15a71c09
danielhemon <danielhemon1@gmail.com>
2024-09-14 14:47:13 -0500

    04 change prod db url

--- src/config.js
+++ src/config.js
@@ -2,6 +2,5 @@ import dotenv from 'dotenv'
 
 const dotenvres = dotenv.config();
 export default {
-    mongoDbUrl: process.env.MONGODB_URI || 'mongodb://mongo:deaECUxvidNbIOavEVuZApPulMGzJFGG@junction.proxy.rlwy.net:18916',
+    mongoDbUrl: process.env.MONGODB_URI || 'mongodb://Localhost/Registration',
 };
-
