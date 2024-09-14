b17cd764d162be9c73186f52561f65bf79261cd9
danielhemon <danielhemon1@gmail.com>
2024-09-14 12:27:44 -0500

    03 conexion a db prod

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ import dotenv from 'dotenv'
 
 const dotenvres = dotenv.config();
 export default {
-    mongoDbUrl: process.env.MONGODB_URI || 'mongodb://Localhost/Registration',
+    mongoDbUrl: process.env.MONGODB_URI || 'mongodb://mongo:deaECUxvidNbIOavEVuZApPulMGzJFGG@junction.proxy.rlwy.net:18916',
 };
 
